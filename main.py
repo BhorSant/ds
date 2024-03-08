ca6c8addf06ec6c1fba366cab57e0567698b116b
Santosh Bhor <91824340+BhorSant@users.noreply.github.com>
2024-03-08 20:15:58 +0530

    Data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from src.mlProject import logger
+import os
+from pathlib import Path
+from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("My name is Santosh Bhor !")
+STAGE_NAME = "Data Ingestion Stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+finally:
+    pass
