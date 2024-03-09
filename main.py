addc60ffeefbc5d2759e8eec65da103fa06acbc6
Santosh Bhor <91824340+BhorSant@users.noreply.github.com>
2024-03-09 11:50:27 +0530

    trail to main.py

--- main.py
+++ main.py
@@ -1,38 +1,66 @@
-import os
-from pathlib import Path
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_trasformation import DataTransformationTrainingPipeline
+from mlProject.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+# from mlProject.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 
-STAGE_NAME = "Data Ingestion Stage"
+
+
+STAGE_NAME = "Data Ingestion stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
-finally:
-    pass
+        logger.exception(e)
+        raise e
 
 
-STAGE_NAME = "Data Validation Stage"
+
+STAGE_NAME = "Data Validation stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataValidationTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
 
-STAGE_NAME = "Data Transformation Stage"
+
+
+STAGE_NAME = "Data Transformation stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataTransformationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-    logger.exception(e)
-    raise e
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME = "Model Trainer stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = ModelTrainerTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+# STAGE_NAME = "Model evaluation stage"
+# try:
+#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+#    data_ingestion = ModelEvaluationTrainingPipeline()
+#    data_ingestion.main()
+#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+# except Exception as e:
+#         logger.exception(e)
+#         raise e
--- src/mlProject/config/configuration.py
+++ src/mlProject/config/configuration.py
@@ -1,6 +1,6 @@
 from mlProject.constants import *
 from mlProject.utils.common import read_yaml, create_directories
-from mlProject.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
+from mlProject.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -45,13 +45,42 @@ class ConfigurationManager:
 
         return data_validation_config
 
-    def get_data_transformation_config(self) -> DataTransformationConfig:
-        config = self.config.data_transformation
+def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema =  self.schema.TARGET_COLUMN
+
         create_directories([config.root_dir])
 
-        data_transformation_config = DataTransformationConfig(
+        model_trainer_config = ModelTrainerConfig(
             root_dir=config.root_dir,
-            data_path=config.data_path
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+            
         )
 
-        return data_transformation_config
+        return model_trainer_config
+
+def get_model_trainer_config(self) -> ModelTrainerConfig:
+    config = self.config.model_trainer
+    params = self.params.ElasticNet
+    schema =  self.schema.TARGET_COLUMN
+
+    create_directories([config.root_dir])
+
+    model_trainer_config = ModelTrainerConfig(
+        root_dir=config.root_dir,
+        train_data_path = config.train_data_path,
+        test_data_path = config.test_data_path,
+        model_name = config.model_name,
+        alpha = params.alpha,
+        l1_ratio = params.l1_ratio,
+        target_column = schema.name
+            
+        )
+
+        return model_trainer_config
